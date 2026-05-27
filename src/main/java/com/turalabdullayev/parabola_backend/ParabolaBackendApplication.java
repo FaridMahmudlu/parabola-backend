c8952e1b4187f3580bb055f991f4a265cd9d4495
Tural20000 <tabdullayev020@gmail.com>
2026-05-27 16:07:53 +0400

    Initial commit

---
+++ src/main/java/com/turalabdullayev/parabola_backend/ParabolaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.turalabdullayev.parabola_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParabolaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParabolaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turalabdullayev/parabola_backend/ParabolaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turalabdullayev.parabola_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParabolaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
