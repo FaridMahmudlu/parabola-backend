1bfec41d337bf36768f6d6797e4d6e3744c7ed0e
Tural20000 <tabdullayev020@gmail.com>
2026-05-29 19:49:13 +0400

    Add picture and file

--- src/main/java/com/turalabdullayev/parabola_backend/controller/ProductController.java
+++ src/main/java/com/turalabdullayev/parabola_backend/controller/ProductController.java
@@ -1,8 +1,15 @@
 package com.turalabdullayev.parabola_backend.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,10 +17,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.turalabdullayev.parabola_backend.entity.Product;
 import com.turalabdullayev.parabola_backend.service.ProductService;
 
@@ -28,15 +37,37 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public class ProductController {
 
 	private final ProductService productService;
+	private final String UPLOAD_DIR = "./uploads/";
 
 	public ProductController(ProductService productService) {
 		this.productService = productService;
 	}
 
-	@PostMapping
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	@PreAuthorize("hasRole('SELLER')")
-	@Operation(summary = "Yeni geyim əlavə et (Satıcı)", description = "Sistemə satıcı rolu ilə daxil olmuş butiklər tərəfindən yeni geyim və onun S, M, L ölçü santimetrlərini yükləyir.")
-	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+	@Operation(summary = "Yeni geyim və Real Şəkil əlavə et (Satıcı)", description = "Sistemə satıcı rolu ilə daxil olmuş butiklər tərəfindən yeni geyim məlumatlarını və kompüterdən seçilən real şəkil faylını yükləyir.")
+	public ResponseEntity<Product> createProduct(@RequestPart("product") String productJson,
+			@RequestPart("image") MultipartFile file) throws IOException {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		Product product = objectMapper.readValue(productJson, Product.class);
+
+		Path uploadPath = Paths.get(UPLOAD_DIR);
+		if (!Files.exists(uploadPath)) {
+			Files.createDirectories(uploadPath);
+		}
+
+		String originalFileName = file.getOriginalFilename();
+		String fileExtension = originalFileName != null ? originalFileName.substring(originalFileName.lastIndexOf("."))
+				: ".jpg";
+		String uniqueFileName = UUID.randomUUID().toString() + fileExtension;
+
+		Path filePath = uploadPath.resolve(uniqueFileName);
+		Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+		String fileUrl = "http://localhost:8080/uploads/" + uniqueFileName;
+		product.setImageUrl(fileUrl);
+
 		Product savedProduct = productService.saveProduct(product);
 		return ResponseEntity.ok(savedProduct);
 	}
