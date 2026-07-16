d4354d7a7dbb10a9d6d9e337cea5f100362bd59a
F3rid.rrr <seid2008ferid@gmail.com>
2026-07-16 16:43:41 +0400

    fix: allow root endpoint / in security config to fix cron-job default ping failures

--- src/main/java/com/turalabdullayev/parabola_backend/controller/PingController.java
+++ src/main/java/com/turalabdullayev/parabola_backend/controller/PingController.java
@@ -2,14 +2,13 @@ package com.turalabdullayev.parabola_backend.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/health")
 public class PingController {
 
-    @GetMapping
+    @GetMapping({"/", "/api/v1/health"})
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok("OK");
     }
